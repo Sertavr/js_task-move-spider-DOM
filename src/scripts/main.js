fa4f77572fc14994ff6015866969925e6ac81fe6
Sertavr <pampuh.net@gmail.com>
2025-11-06 14:35:39 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,25 +15,25 @@ document.addEventListener('click', (e) => {
   const leftX = aboutWall.left + wall.clientLeft;
   const rightX = leftX + wall.clientWidth;
 
-  let centerX = e.clientX - leftX - spider.clientWidth / 2;
-  let centerY = e.clientY - topY - spider.clientHeight / 2;
+  let spiderX = e.clientX - leftX - spider.clientWidth / 2;
+  let spiderY = e.clientY - topY - spider.clientHeight / 2;
 
   if (e.clientX - leftX < spider.clientWidth / 2) {
-    centerX = 0;
+    spiderX = 0;
   }
 
   if (e.clientY - topY < spider.clientHeight / 2) {
-    centerY = 0;
+    spiderY = 0;
   }
 
   if (e.clientX > rightX - spider.clientWidth) {
-    centerX = wall.clientWidth - spider.clientWidth;
+    spiderX = wall.clientWidth - spider.clientWidth;
   }
 
   if (e.clientY > bottomY - spider.clientHeight) {
-    centerY = wall.clientHeight - spider.clientHeight;
+    spiderY = wall.clientHeight - spider.clientHeight;
   }
 
-  spider.style.left = `${centerX}px`;
-  spider.style.top = `${centerY}px`;
+  spider.style.left = `${spiderX}px`;
+  spider.style.top = `${spiderY}px`;
 });
